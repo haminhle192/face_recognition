9f38154757c886999538826650875ac20a4aa7c2
vietbq <vietbq@nal.vn>
2019-05-17 11:22:02 +0700

    Server side camera

---
+++ src/server_camera.py
@@ -0,0 +1,24 @@
+import io
+import socket
+import struct
+
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8000))
+server_socket.listen(0)
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+frame = 0
+try:
+    while True:
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        image_stream.seek(0)
+        frame +=1
+finally:
+    connection.close()
+    server_socket.close()
+    print(frame)
