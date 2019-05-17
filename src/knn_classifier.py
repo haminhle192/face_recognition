e86d726d0d3ea6c90f1d3691fe50ed4e0e43723e
Tran The Anh <trantheanh26061989@gmail.com>
2019-05-17 18:14:24 +0700

    Testing performance.

--- src/knn_classifier.py
+++ src/knn_classifier.py
@@ -10,6 +10,7 @@ import src.encoder1 as encoder
 import src.kNN as kNN
 import pandas as pd
 import numpy as np
+import time
 
 
 class KNNClassifier:
@@ -19,7 +20,7 @@ class KNNClassifier:
             saved_classifier_dir=os.path.dirname(__file__) + "/../saved_classifiers",
             classifier_filename="knn_classifier.pkl",
             n_neighbors=1,
-            default_threshold=0.7
+            default_threshold=0.901
     ):
         self.n_neighbors = n_neighbors
         self.data_dir = data_dir
@@ -58,6 +59,61 @@ class KNNClassifier:
                     pickle.dump((model, class_names), outfile)
                 print('Saved classifier model to file "%s"' % classifier_filename_exp)
 
+    def split_data(self, data_file, label_file, out1_file, out2_file):
+        df = pd.DataFrame(np.load(data_file))
+        df[512] = np.load(label_file)
+        df = df.sort_values(by=[512]).groupby([512])
+        train_df = pd.DataFrame(np.zeros((0, 513)))
+        test_df = pd.DataFrame(np.zeros((0, 513)))
+        for n, values in df:
+            print(n)
+            # st = time.time()
+            d = pd.DataFrame(values)
+            d = d.sample(frac=1).reset_index(drop=True)
+            len = d.shape[0]
+            if len >= 20:
+                train_df = train_df.append(d.iloc[0:4], ignore_index=True)
+                test_df = test_df.append(d.iloc[4:len], ignore_index=True)
+            else:
+                # train_df = train_df.append(d.iloc[0:len//2], ignore_index=True)
+                # test_df = test_df.append(d.iloc[len//2:len], ignore_index=True)
+                test_df = test_df.append(d, ignore_index=True)
+            # print(time.time() - st)
+
+        np.save(out1_file, train_df.values)
+        np.save(out2_file, test_df.values)
+
+    def test_performance(self, train_file, test_file):
+        st = time.time()
+        print("============Loading data===============")
+        train_df = pd.DataFrame(np.load(train_file))
+        test_df = pd.DataFrame(np.load(test_file))
+        train_data = train_df.drop(labels=[512], axis=1).values
+        train_labels = np.ndarray.tolist((train_df.values[:, -1]).astype(int))
+        test_data = test_df.drop(labels=[512], axis=1).values
+        test_labels = np.ndarray.tolist((test_df.values[:, -1]).astype(int))
+        print("============End Loading data===============", time.time() - st)
+        st = time.time()
+        print("============Calculate thresholds===============")
+        thresholds = self.cal_thresholds(train_data, train_labels)
+        # thresholds = np.zeros_like(thresholds) + 0.7
+        print(thresholds)
+        print("============End Calculate thresholds===============", time.time() - st)
+        st = time.time()
+        print("============Test performance===============")
+        model = kNN.kNN(train_data, train_labels, thresholds, self.n_neighbors)
+        predictions = model.predict(test_data)
+        print(predictions)
+        print(test_labels)
+        accuracy = np.mean(np.equal(predictions, test_labels))
+        fp = (((predictions - test_labels)!=0)*predictions)
+        fp = fp[fp != 0]
+        fp = fp[fp != np.max(test_labels)+1]
+        print(fp)
+        print('Accuracy: %.3f' % accuracy)
+        print('False Positive: %.3f' % (len(fp)/predictions.shape[0]))
+        print("============End Test performance===============", time.time() - st)
+
     def cal_thresholds(self, embeddings, labels):
         df = pd.DataFrame(embeddings)
         df[512] = labels
@@ -67,13 +123,13 @@ class KNNClassifier:
         print("=========>threshold_class", threshold_class)
         thresholds = np.zeros((1, max_label + 1))
         for cls, group in emb_by_classes:
-            print("========================>", cls)
+            # print("========================>", cls)
             group = group.drop(512, axis=1)
             if np.shape(group.values)[0] > 1:
-                thresholds[0][cls] = self.get_threshold(group.values, self.default_threshold)
+                thresholds[0][cls] = KNNClassifier.get_threshold(group.values, self.default_threshold)
             else:
                 thresholds[0][cls] = np.min([threshold_class, self.default_threshold])
-        print(thresholds)
+        # print(thresholds)
         return thresholds
 
     @staticmethod
@@ -90,8 +146,9 @@ class KNNClassifier:
 
         result = np.linalg.norm(result, axis=2)
         result = result.flatten()
+        print("++++++++++++++++++++++")
         print(result)
         result = result[result != 0]
-        result = np.append(result, [default_threshold], axis=0)
-        result = np.min(result)
+        result = np.max(result, axis=0)
+        result = np.min(np.array([result, default_threshold]))
         return result
--- src/try_my_facenet.py
+++ src/try_my_facenet.py
@@ -4,18 +4,27 @@ from __future__ import print_function
 
 import src.face as face
 import os
+import src.knn_classifier as classifier
+import src.encoder1 as encoder1
 import time
 
 def main():
-    start_time = time.time()
+    # start_time = time.time()
     # face.Face.export_detection_for_training_data()
     # face.Face.train()
-    f = face.Face()
-    print("Time to load model: %s" % str(time.time() - start_time))
-    predict_time = time.time()
-    for i in range(0, 1):
-        result = f.predict(os.path.dirname(__file__) + "/../data/prediction/" + str(i) + ".png")
-    print("Predict time : %s" % str(time.time() - predict_time))
+    # f = face.Face()
+
+    # encoder1.Encoder().export_embeddings("E:\Data\Test", "E:\Data\embeddings2.npy", "E:\Data\labels2.npy",  "E:\Data\label1_string2.npy")
+    knn = classifier.KNNClassifier()
+    knn.split_data("E:\Data\embeddings2.npy", "E:\Data\labels2.npy", "E:\Data\\train_embeddings2.npy", "E:\Data\\test_embeddings2.npy")
+    knn.test_performance("E:\Data\\train_embeddings2.npy", "E:\Data\\test_embeddings2.npy")
+    # knn.test_performance("E:\Data\CASIA_train_embeddings.npy", "E:\Data\CASIA_test_embeddings.npy")
+
+    # print("Time to load model: %s" % str(time.time() - start_time))
+    # predict_time = time.time()
+    # for i in range(0, 1):
+    #     result = f.predict(os.path.dirname(__file__) + "/../data/prediction/" + str(i) + ".png")
+    # print("Predict time : %s" % str(time.time() - predict_time))
 
 if __name__ == '__main__':
     main()
