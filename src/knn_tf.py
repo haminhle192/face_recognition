51da448978253d76c1c1f39af9d12ffdc3bfa9fa
Tran The Anh <trantheanh26061989@gmail.com>
2019-05-27 18:00:02 +0700

    Testing knn by tf.

--- src/knn_tf.py
+++ src/knn_tf.py
@@ -8,24 +8,22 @@ class kNN:
     def __init__(self,
                  data_train_X,
                  data_train_Y,
-                 data_test_X,
                  thresholds,
                  k=1):
         self.data_train_X = data_train_X
         self.data_train_Y = data_train_Y
-        self.data_test_X = data_test_X
         self.k = k
         self.thresholds = thresholds
         self.nof_class = np.max(data_train_Y) + 1
 
-        sess = tf.Session()
-        with sess.as_default():
+        self.sess = tf.Session()
+        with self.sess.as_default():
             self.x_train, self.y_train, self.x_test, self.prediction = self._build()
 
     def _build(self):
         feature_number = len(self.data_train_X[0])
         x_train = tf.placeholder(shape=[None, feature_number], dtype=tf.float32)
-        y_train = tf.placeholder(shape=[None, len(self.data_train_Y[0])], dtype=tf.float32)
+        y_train = tf.placeholder(shape=[None], dtype=tf.float32)
         x_test = tf.placeholder(shape=[None, feature_number], dtype=tf.float32)
 
         #euclidean distance
@@ -33,32 +31,39 @@ class kNN:
 
         # nearest k points
         top_k_values, top_k_indices = tf.nn.top_k(tf.negative(distance), k=self.k)
-        top_k_values = tf.negative(top_k_values)
-        top_k_labels = tf.gather(y_train, top_k_indices)
-        top_k_thresholds = tf.gather(self.thresholds, top_k_labels)
-
-        top_k_values = tf.multiply(tf.less_equal(top_k_values, top_k_thresholds), top_k_values)
-        sigma = .5
-        top_k_values = tf.exp(tf.div(tf.multiply(top_k_values, top_k_values), sigma))
-
+        top_k_labels = tf.to_int32(tf.gather(y_train, top_k_indices))
+        # top_k_thresholds = tf.to_float(tf.reshape(tf.gather(self.thresholds, top_k_labels), (-1, self.nof_class)))
+        top_k_thresholds = tf.to_float(tf.reshape(tf.gather(self.thresholds, top_k_labels, axis=1), (-1, self.k)))
         top_k_labels_one_hot = tf.one_hot(top_k_labels, self.nof_class)
-        weight_by_labels = tf.matmul(tf.expand_dims(top_k_values, 1), top_k_labels_one_hot)
-        weight_by_labels = tf.reshape(weight_by_labels, (None, self.nof_class))
+
+        top_k_values = tf.negative(top_k_values)
+
+        top_k_values = tf.multiply(tf.to_float(tf.less_equal(top_k_values, top_k_thresholds)), top_k_values)
+        self.top_k_values = top_k_values
+        sigma = .5
+        top_k_values = tf.reshape(tf.exp(tf.div(tf.multiply(top_k_values, top_k_values), sigma)), (-1, 1, self.k))
+
+        weight_by_labels = tf.matmul(a=top_k_values, b=top_k_labels_one_hot)
+        weight_by_labels = tf.reshape(weight_by_labels, (-1, self.nof_class))
 
         prediction = tf.multiply(
-            tf.greater_equal(weight_by_labels, tf.tile(tf.reduce_max(weight_by_labels, axis=1, keepdims=True), (1, weight_by_labels.shape[1]))),
-            tf.greater(weight_by_labels, tf.tile(tf.reduce_min(weight_by_labels, axis=11, keepdims=True)), (1, weight_by_labels.shape[1]))
+            tf.to_int32(tf.greater_equal(weight_by_labels, tf.tile(tf.reduce_max(weight_by_labels, axis=1, keepdims=True), (1, weight_by_labels.shape[1])))),
+            tf.to_int32(tf.greater(weight_by_labels, tf.tile(tf.reduce_min(weight_by_labels, axis=1, keepdims=True), (1, weight_by_labels.shape[1]))))
         )
 
         return x_train, y_train, x_test, prediction
 
-    def fit(self, sess):
-        prediction = sess.run(self.prediction(), feed_dict={self.x_train: self.data_train_X,
-                                                            self.x_test: self.data_test_X,
-                                                            self.y_train: self.data_train_Y})
+    def fit(self, data_test_X):
+        print('TEST DATA SHAPE: {}'.format(data_test_X.shape))
+        print('TRAIN DATA SHAPE: {}'.format(self.data_train_X.shape))
+        prediction = self.sess.run(self.prediction, feed_dict={self.x_train: self.data_train_X,
+                                                                 self.x_test: data_test_X,
+                                                                 self.y_train: self.data_train_Y})
+        print('CURRENT TEST {}'.format(prediction.shape))
 
         mlb = MultiLabelBinarizer()
-        mlb.fit_transform(self.data_train_Y)
+        print(self.data_train_Y)
+        mlb.fit_transform([set(self.data_train_Y)])
         return mlb.inverse_transform(prediction)
 
     @staticmethod
