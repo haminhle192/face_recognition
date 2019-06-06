d6f29271d2863e337c5373bf173a89d1a3fe7294
vietbq <vietbq@nal.vn>
2019-06-06 17:41:18 +0700

    Update detection

---
+++ src/camera_pi_v3.py
@@ -0,0 +1,89 @@
+import io
+import socket
+import struct
+import time
+import threading
+import picamera
+import src.detection as detection
+
+client_socket = socket.socket()
+client_socket.connect(('192.168.1.212', 8000))
+connection = client_socket.makefile('wb')
+try:
+    connection_lock = threading.Lock()
+    pool = []
+    pool_lock = threading.Lock()
+
+    class ImageStreamer(threading.Thread):
+        def __init__(self):
+            super(ImageStreamer, self).__init__()
+            print("Starting Image Streamer")
+            self.detection = detection.Detection()
+            self.stream = io.BytesIO()
+            self.event = threading.Event()
+            self.terminated = False
+            self.start()
+
+        def run(self):
+            # This method runs in a background thread
+            while not self.terminated:
+                if self.event.wait(1):
+                    try:
+                        with connection_lock:
+                            connection.write(struct.pack('<L', self.stream.tell()))
+                            connection.flush()
+                            self.stream.seek(0)
+                            img = self.stream.read()
+                            faces = self.detection.find_faces(img)
+                            print(faces)
+                            connection.write(img)
+                    finally:
+                        self.stream.seek(0)
+                        self.stream.truncate()
+                        self.event.clear()
+                        with pool_lock:
+                            pool.append(self)
+
+    count = 0
+    start = time.time()
+    finish = time.time()
+
+    def streams():
+        global count, finish
+        while finish - start < 60:
+            with pool_lock:
+                if len(pool)==0:
+                    continue
+                streamer = pool.pop()
+            yield streamer.stream
+            streamer.event.set()
+            count += 1
+            finish = time.time()
+
+    with picamera.PiCamera() as camera:
+        pool = [ImageStreamer() for i in range(2)]
+        camera.resolution = (480, 320)
+        # Set the framerate appropriately; too fast and we'll starve the
+        # pool of streamers and crash the script
+        #camera.framerate = 48
+        #camera.start_preview()
+        time.sleep(2)
+        camera.capture_sequence(streams(), 'jpeg', use_video_port=True)
+    # Shut down the streamers in an orderly fashion
+    while pool:
+        with pool_lock:
+            streamer = pool.pop()
+        streamer.terminated = True
+        streamer.join()
+
+    # Write the terminating 0-length: to the connection to let the server
+    # know we're done
+    with connection_lock:
+        connection.write(struct.pack('<L', 0))
+
+finally:
+    connection.close()
+    client_socket.close()
+
+print('Sent %d images in %.2f seconds at %.2ffps' % (
+    count, finish-start, count / (finish-start)))
--- src/detection.py
+++ src/detection.py
@@ -6,10 +6,8 @@ import os
 
 import numpy as np
 import tensorflow as tf
-from scipy import misc
 import src.align.detect_face as detect_face
 import random
-import src.facenet as facenet
 from time import sleep
 import shutil
 
@@ -44,123 +42,115 @@ class Detection:
             with sess.as_default():
                 return detect_face.create_mtcnn(sess, None)
 
-    def export_detection(
-        self,
-        input_dir=os.path.dirname(__file__) + "/../data/raw",
-        output_dir=os.path.dirname(__file__) + "/../data/raw_aligned",
-        detect_multiple_faces=False
-    ):
-        print("Starting detect face ...")
-        sleep(random.random())
-        data_set = facenet.get_dataset(input_dir)
-        output_dir = os.path.expanduser(output_dir)
+    # def export_detection(
+    #     self,
+    #     input_dir=os.path.dirname(__file__) + "/../data/raw",
+    #     output_dir=os.path.dirname(__file__) + "/../data/raw_aligned",
+    #     detect_multiple_faces=False
+    # ):
+    #     print("Starting detect face ...")
+    #     sleep(random.random())
+    #     data_set = facenet.get_dataset(input_dir)
+    #     output_dir = os.path.expanduser(output_dir)
+    #
+    #     if os.path.exists(output_dir):
+    #         shutil.rmtree(output_dir)
+    #
+    #     os.makedirs(output_dir)
+    #     print("Created output_dir.")
+    #
+    #     # Store some git revision info in a text file in the log directory
+    #     src_path, _ = os.path.split(os.path.realpath(__file__))
+    #     facenet.store_revision_info(src_path, output_dir, ' '.join([input_dir, output_dir, str(detect_multiple_faces)]))
+    #
+    #     # Add a random key to the filename to allow alignment using multiple processes
+    #     random_key = np.random.randint(0, high=99999)
+    #     bounding_boxes_filename = os.path.join(output_dir, 'bounding_boxes_%05d.txt' % random_key)
+    #
+    #     with open(bounding_boxes_filename, "w") as text_file:
+    #         nrof_images_total = 0
+    #         nrof_successfully_aligned = 0
+    #         for cls in data_set:
+    #             output_class_dir = os.path.join(output_dir, cls.name)
+    #             if not os.path.exists(output_class_dir):
+    #                 os.makedirs(output_class_dir)
+    #             for image_path in cls.image_paths:
+    #                 nrof_images_total += 1
+    #                 filename = os.path.splitext(os.path.split(image_path)[1])[0]
+    #                 output_filename = os.path.join(output_class_dir, filename + '.png')
+    #
+    #                 if not os.path.exists(output_filename):
+    #                     try:
+    #                         img = misc.imread(image_path)
+    #                     except (IOError, ValueError, IndexError) as e:
+    #                         error_message = '{}: {}'.format(image_path, e)
+    #                         print(error_message)
+    #                     else:
+    #                         if img.ndim < 2:
+    #                             print('Unable to align "%s"' % image_path)
+    #                             text_file.write('ERROR1 %s\n' % output_filename)
+    #                             continue
+    #
+    #                         faces = self.find_faces(img)
+    #                         nrof_faces = len(faces)
+    #                         if nrof_faces == 0:
+    #                             print('Unable to align "%s"' % image_path)
+    #                             text_file.write('ERROR2 %s\n' % output_filename)
+    #                             continue
+    #                         if nrof_faces == 1:
+    #                             nrof_successfully_aligned += 1
+    #                             filename_base, file_extension = os.path.splitext(output_filename)
+    #                             output_filename_n = "{}{}".format(filename_base, file_extension)
+    #                             misc.imsave(output_filename_n, faces[0].image)
+    #                             bb = faces[0].bounding_box
+    #                             text_file.write(
+    #                                 '%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
+    #                         else:
+    #                             if detect_multiple_faces:
+    #                                 nrof_successfully_aligned += 1
+    #                                 for i in range(nrof_faces):
+    #                                     output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
+    #                                     misc.imsave(output_filename_n, faces[i].image)
+    #                                     bb = faces[i].bounding_box
+    #                                     text_file.write(
+    #                                         '%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
+    #                             else:
+    #                                 print('Unable to align "%s"' % image_path)
+    #                                 text_file.write('ERROR3 %s\n' % output_filename)
+    #
+    #     print('Total number of images: %d' % nrof_images_total)
+    #     print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
+    #     print("End detect face for training data.")
 
-        if os.path.exists(output_dir):
-            shutil.rmtree(output_dir)
-
-        os.makedirs(output_dir)
-        print("Created output_dir.")
-
-        # Store some git revision info in a text file in the log directory
-        src_path, _ = os.path.split(os.path.realpath(__file__))
-        facenet.store_revision_info(src_path, output_dir, ' '.join([input_dir, output_dir, str(detect_multiple_faces)]))
-
-        # Add a random key to the filename to allow alignment using multiple processes
-        random_key = np.random.randint(0, high=99999)
-        bounding_boxes_filename = os.path.join(output_dir, 'bounding_boxes_%05d.txt' % random_key)
-
-        with open(bounding_boxes_filename, "w") as text_file:
-            nrof_images_total = 0
-            nrof_successfully_aligned = 0
-            for cls in data_set:
-                output_class_dir = os.path.join(output_dir, cls.name)
-                if not os.path.exists(output_class_dir):
-                    os.makedirs(output_class_dir)
-                for image_path in cls.image_paths:
-                    nrof_images_total += 1
-                    filename = os.path.splitext(os.path.split(image_path)[1])[0]
-                    output_filename = os.path.join(output_class_dir, filename + '.png')
-
-                    if not os.path.exists(output_filename):
-                        try:
-                            img = misc.imread(image_path)
-                        except (IOError, ValueError, IndexError) as e:
-                            error_message = '{}: {}'.format(image_path, e)
-                            print(error_message)
-                        else:
-                            if img.ndim < 2:
-                                print('Unable to align "%s"' % image_path)
-                                text_file.write('ERROR1 %s\n' % output_filename)
-                                continue
-
-                            faces = self.find_faces(img)
-                            nrof_faces = len(faces)
-                            if nrof_faces == 0:
-                                print('Unable to align "%s"' % image_path)
-                                text_file.write('ERROR2 %s\n' % output_filename)
-                                continue
-                            if nrof_faces == 1:
-                                nrof_successfully_aligned += 1
-                                filename_base, file_extension = os.path.splitext(output_filename)
-                                output_filename_n = "{}{}".format(filename_base, file_extension)
-                                misc.imsave(output_filename_n, faces[0].image)
-                                bb = faces[0].bounding_box
-                                text_file.write(
-                                    '%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
-                            else:
-                                if detect_multiple_faces:
-                                    nrof_successfully_aligned += 1
-                                    for i in range(nrof_faces):
-                                        output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
-                                        misc.imsave(output_filename_n, faces[i].image)
-                                        bb = faces[i].bounding_box
-                                        text_file.write(
-                                            '%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
-                                else:
-                                    print('Unable to align "%s"' % image_path)
-                                    text_file.write('ERROR3 %s\n' % output_filename)
-
-        print('Total number of images: %d' % nrof_images_total)
-        print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
-        print("End detect face for training data.")
-
-    def find_faces_from_paths(self, image_paths):
-        nrof_samples = len(image_paths)
-        faces = []
-        count_per_image = []
-
-        for i in xrange(nrof_samples):
-            img = misc.imread(os.path.expanduser(image_paths[i]))
-            img_size = np.asarray(img.shape)[0:2]
-            bounding_boxes, _ = detect_face.detect_face(img, self.minsize, self.pnet, self.rnet, self.onet,
-                                                        self.threshold, self.factor)
-            count_per_image.append(len(bounding_boxes))
-
-            for bb in bounding_boxes:
-                face = Face()
-                face.container_image = img
-                face.bounding_box = np.zeros(4, dtype=np.int32)
-
-                face.bounding_box[0] = np.maximum(bb[0] - self.face_crop_margin / 2, 0)
-                face.bounding_box[1] = np.maximum(bb[1] - self.face_crop_margin / 2, 0)
-                face.bounding_box[2] = np.minimum(bb[2] + self.face_crop_margin / 2, img_size[1])
-                face.bounding_box[3] = np.minimum(bb[3] + self.face_crop_margin / 2, img_size[0])
-                cropped = img[face.bounding_box[1]:face.bounding_box[3], face.bounding_box[0]:face.bounding_box[2], :]
-                face.image = misc.imresize(cropped, (self.face_crop_size, self.face_crop_size), interp='bilinear')
-
-                faces.append(face)
-
-        return faces, count_per_image, nrof_samples
+    # def find_faces_from_paths(self, image_paths):
+    #     nrof_samples = len(image_paths)
+    #     faces = []
+    #     count_per_image = []
+    #
+    #     for i in xrange(nrof_samples):
+    #         img = misc.imread(os.path.expanduser(image_paths[i]))
+    #         img_size = np.asarray(img.shape)[0:2]
+    #         bounding_boxes, _ = detect_face.detect_face(img, self.minsize, self.pnet, self.rnet, self.onet,
+    #                                                     self.threshold, self.factor)
+    #         count_per_image.append(len(bounding_boxes))
+    #
+    #         for bb in bounding_boxes:
+    #             face = Face()
+    #             face.container_image = img
+    #             face.bounding_box = np.zeros(4, dtype=np.int32)
+    #
+    #             face.bounding_box[0] = np.maximum(bb[0] - self.face_crop_margin / 2, 0)
+    #             face.bounding_box[1] = np.maximum(bb[1] - self.face_crop_margin / 2, 0)
+    #             face.bounding_box[2] = np.minimum(bb[2] + self.face_crop_margin / 2, img_size[1])
+    #             face.bounding_box[3] = np.minimum(bb[3] + self.face_crop_margin / 2, img_size[0])
+    #             cropped = img[face.bounding_box[1]:face.bounding_box[3], face.bounding_box[0]:face.bounding_box[2], :]
+    #             face.image = misc.imresize(cropped, (self.face_crop_size, self.face_crop_size), interp='bilinear')
+    #
+    #             faces.append(face)
+    #
+    #     return faces, count_per_image, nrof_samples
 
     def find_faces(self, image):
-        if image.ndim < 2:
-            return []
-        if image.ndim == 2:
-            image = facenet.to_rgb(image)
-
-        if image.ndim > 3:
-            image = facenet.to_rgb2(image)
-
         image = image[:, :, 0:3]
 
         faces = []
@@ -178,7 +168,8 @@ class Detection:
             face.bounding_box[2] = np.minimum(bb[2] + self.face_crop_margin / 2, img_size[1])
             face.bounding_box[3] = np.minimum(bb[3] + self.face_crop_margin / 2, img_size[0])
             cropped = image[face.bounding_box[1]:face.bounding_box[3], face.bounding_box[0]:face.bounding_box[2], :]
-            face.image = misc.imresize(cropped, (self.face_crop_size, self.face_crop_size), interp='bilinear')
+            face.image = cropped
+            # face.image = misc.imresize(cropped, (self.face_crop_size, self.face_crop_size), interp='bilinear')
 
             faces.append(face)
 
