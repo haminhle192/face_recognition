b07937e81e3d3199f5045147fd1b809670669663
Tran The Anh <trantheanh26061989@gmail.com>
2019-06-06 17:20:01 +0700

    Fixing try_my_facenet.py

--- src/try_my_facenet.py
+++ src/try_my_facenet.py
@@ -11,19 +11,21 @@ import time
 def main():
     # start_time = time.time()
     # face.Face.export_detection_for_training_data()
-    # face.Face.train()
-    # f = face.Face()
+    face.Face.train()
+    f = face.Face()
 
     # encoder1.Encoder().export_embeddings("E:\Data\Test2", "E:\Data\embeddings3.npy", "E:\Data\labels3.npy",  "E:\Data\label1_string3.npy")
-    knn = classifier.KNNClassifier()
-    # knn.split_data("E:\Data\embeddings2.npy", "E:\Data\labels2.npy", "E:\Data\\train_embeddings2.npy", "E:\Data\\test_embeddings2.npy")
-    knn.test_performance("E:\Data\\train_embeddings3.npy", "E:\Data\\test_embeddings3.npy")
-    # knn.test_performance("E:\Data\CASIA_train_embeddings.npy", "E:\Data\CASIA_test_embeddings.npy")
+    # knn = classifier.KNNClassifier()
+    # knn.split_data("E:\Data\embeddings3.npy", "E:\Data\labels3.npy", "E:\Data\\train_embeddings3.npy", "E:\Data\\test_embeddings3.npy")
+    # knn.split_data("E:\Data\CASIA-embedded.npy", "E:\Data\CASIA-labels.npy", "E:\Data\CASIA_train_embeddings1.npy", "E:\Data\CASIA_test_embeddings1.npy")
+    # knn.test_performance("E:\Data\\train_embeddings2.npy", "E:\Data\\test_embeddings2.npy")
+    # knn.test_performance("E:\Data\CASIA_train_embeddings1.npy", "E:\Data\CASIA_test_embeddings1.npy")
 
     # print("Time to load model: %s" % str(time.time() - start_time))
     # predict_time = time.time()
-    # for i in range(0, 1):
-    #     result = f.predict(os.path.dirname(__file__) + "/../data/prediction/" + str(i) + ".png")
+    for i in range(0, 1):
+        result = f.predict(os.path.dirname(__file__) + "/../data/prediction/" + str(i) + ".png")
+        print(result)
     # print("Predict time : %s" % str(time.time() - predict_time))
 
 if __name__ == '__main__':
