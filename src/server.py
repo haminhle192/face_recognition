896fabada870b5754efa68d43902472f0da533e0
vietbq <vietbq@nal.vn>
2019-07-14 08:52:09 +0700

    Update server socket

--- src/server.py
+++ src/server.py
@@ -14,21 +14,27 @@ import json
 
 class Server:
     def __init__(self):
-        self.predictor = face.Face()
         self.server_socket = socket.socket()
         self.server_socket.bind(('0.0.0.0', 8989))
         print('Start server')
+        self.predictor = face.Face()
+        self.predictor.predict1(np.empty((160, 160, 3), dtype=np.uint8))
+        print('Waiting for Client')
         self.server_socket.listen(0)
 
     def listen_client(self):
-        connection = self.server_socket.accept()[0].makefile('rb')
+        conn, addr = self.server_socket.accept()
+        read_stream = conn.makefile('rb')
+        write_stream = conn.makefile('wb')
+        writer = io.BytesIO()
+        print('Client connected')
         try:
             while True:
-                image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-                if not image_len:
-                    continue
+                image_len = struct.unpack('<L', read_stream.read(struct.calcsize('<L')))[0]
+                if image_len == 0:
+                    break
                 image_stream = io.BytesIO()
-                image_stream.write(connection.read(image_len))
+                image_stream.write(read_stream.read(image_len))
                 image_stream.seek(0)
                 img_decoded = self.decode_jpeg(image_stream.getvalue(), (160, 160, 3), dtype=np.uint8)
                 # cv2.imshow('Network Image',img_decoded)
@@ -36,17 +42,23 @@ class Server:
                 # image.verify()
                 response = self.identify_face(img_decoded)
                 print(response)
-                # with io.BytesIO as writer:
-                #     writer.write(response.encode())
-                #     data_length = writer.tell()
-                #     connection.write(struct.pack('<L', data_length))
-                #     writer.seek(0)
-                #     connection.flush()
-                #     connection.write(writer.read(data_length))
-                #     print('Did send response %2f' % data_length)
+                j_response = json.dumps(response)
+                writer.seek(0)
+                writer.write(j_response.encode())
+                data_length = writer.tell()
+                write_stream.write(struct.pack('<L', data_length))
+                writer.seek(0)
+                write_stream.flush()
+                write_stream.write(writer.read(data_length))
+                write_stream.flush()
+        except Exception as e:
+            print(e)
+            print('Disconnect')
         finally:
-            connection.close()
-            self.server_socket.close()
+            write_stream.close()
+            read_stream.close()
+            if self.server_socket is not None:
+                self.server_socket.close()
 
     def decode_jpeg(self, bytestring, shape, dtype):
         img = Image.open(io.BytesIO(bytestring))
